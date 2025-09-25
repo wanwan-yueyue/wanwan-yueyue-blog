7751708fe537000aaea8d17365f076cef7bd7bfe
江畔晚吟时 <1547834680@qq.com>
2025-09-25 19:17:10 +0800

    图片加载，排版样式优化

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitepress'
 import { withMermaid } from 'vitepress-plugin-mermaid'
-import mathjax3 from 'markdown-it-mathjax3'
+// import mathjax3 from 'markdown-it-mathjax3'
 
 // 声明 MathJax 生成的自定义标签（避免 Vue 因未知标签报错）
 const mathCustomElements = [
@@ -94,94 +94,127 @@ const mathCustomElements = [
 ];
 
 export default withMermaid(defineConfig({
-  appearance: true, // 允许用户切换暗色模式
+  	appearance: true, // 允许用户切换暗色模式
+  	titleTemplate: ':title - 江晚',
+  	description: "江畔晚吟时的博客",
+  	lastUpdated: true,		// 最后更新时间戳
 
-  // Mermaid 配置（可选）
-  mermaid: {
-    theme: 'default',
-  },
+  	// Mermaid 配置
+  	mermaid: {
+    	theme: 'default',
+  	},
 
-  // Markdown 配置：启用 LaTeX 解析（通过 markdown-it-mathjax3）
-  markdown: {
-	html:true,
-    lineNumbers: true,
-    config: (md) => {
-      md.use(mathjax3); // 注册 MathJax 插件，解析 $...$ / $$...$$ 包裹的 LaTeX
-    }
-  },
+  	// Markdown 配置：启用 LaTeX 解析（通过 markdown-it-mathjax3）
+  	markdown: {
+		html:true,
+    	lineNumbers: true,
+    	// config: (md) => {
+    	//   md.use(mathjax3); // 注册 MathJax 插件，解析 $...$ / $$...$$ 包裹的 LaTeX
+    	// },
+		math: true,
+		image:{
+			//启用图片懒加载
+			lazyLoading: true
+		}
+  	},
 
-  // Vue 配置：声明 MathJax 标签为「自定义元素」
-  vue: {
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => mathCustomElements.includes(tag)
-      }
-    }
-  },
+  	// Vue 配置：声明 MathJax 标签为「自定义元素」
+  	vue: {
+    	template: {
+      		compilerOptions: {
+        		isCustomElement: (tag) => mathCustomElements.includes(tag)
+      		}
+    	}
+  	},
 
-  // 添加静态资源处理
-  vite:{
-	build: {
-		assetsDir: 'assets',
-		rollupOptions:{
-			output:{
-				manualChunks:undefined,
+  	// 添加静态资源处理
+  	vite:{
+		build: {
+			assetsDir: 'assets',
+			rollupOptions:{
+				output:{
+					manualChunks:undefined,
+				},
 			},
 		},
-	},
-	server: {
-		fs: {
-			allow: ['..']
+		server: {
+			fs: {
+				allow: ['..']
+			}
 		}
-	}
-  },
+  	},
 
-  themeConfig: {
-    siteTitle: "江晚的博客",
-    sidebar: {
+  	themeConfig: {
+    	siteTitle: "江晚的博客",
+    	sidebar: {
 	
-		'/':[// ========== 技术实践 ==========
-      {
-        text: '技术实践',
-        collapsed: false,
-        items: [
-          { text: '第一篇技术笔记', link: '/posts/project/helloworld.md' },
-          { text: 'C51 音乐播放器', link: '/posts/project/C51音乐播放器.md'},
-          { text: 'C51 俄罗斯方块', link: '/posts/project/C51俄罗斯方块.md'}
-        ]
-      },
-      // ========== 学科论文 ==========
-      {
-        text: '学科论文',
-        collapsed: false,    
-        items: [
-          { text: '拉格朗日方程在机械臂中的数学建模', link: '/posts/thesis/Derivations of Lagrange Equation.md' }
-        ]
-      },
-      // ========== 生活随笔 ==========
-      {
-        text: '生活随笔',
-        collapsed: false,    
-        items: [
-          { text: '猫说', link: '/posts/essay/catSaid.md' },
-          { text: '七夕随笔（其一）', link: '/posts/essay/ChineseValentine_Day(first)'},
-          { text: 'Leave or wait', link: '/posts/essay/Leave_or_wait.md'},
-          { text: '杂篇六则', link: '/posts/essay/miscellaneous_notes.md'},
-          { text: '军训随笔', link: '/posts/essay/militaryTraining.md' }
-        ]
-      },
-      // ========== 短篇小说 ==========
-      {
-        text: '短篇小说',
-        collapsed: false,    
-        items: [
-          { text: '余响', link: '/posts/novel/echo.md' },
-		  { text: '三勺石绿，半勺藤黄', link: '/posts/novel/三勺石绿，半勺藤黄.md'},
-		  { text: '衔香记：一瓣栀子渡余生', link: '/posts/novel/衔香记：一瓣栀子渡余生.md' },
-          { text: '未命名故事', link: '/posts/novel/story.md' }
-        ]
-      }]
+			'/':[
+		// ========== 技术实践 ==========
+      			{
+        			text: '技术实践',
+        			collapsed: false,
+        			items: [
+          				{ text: '第一篇技术笔记', link: '/posts/project/helloworld.md' },
+          				{ text: 'C51 音乐播放器', link: '/posts/project/C51音乐播放器.md'},
+          				{ text: 'C51 俄罗斯方块', link: '/posts/project/C51俄罗斯方块.md'}
+        			]
+      			},
+      	// ========== 学科论文 ==========
+      			{
+        			text: '学科论文',
+        			collapsed: false,    
+        			items: [
+          				{ text: '拉格朗日方程在机械臂中的数学建模', link: '/posts/thesis/Derivations of Lagrange Equation.md' }
+        			]
+      			},
+      	// ========== 生活随笔 ==========
+      			{
+        			text: '生活随笔',
+        			collapsed: false,    
+        			items: [
+          				{ text: '猫说', link: '/posts/essay/catSaid.md' },
+          				{ text: '七夕随笔（其一）', link: '/posts/essay/ChineseValentine_Day(first)'},
+          				{ text: 'Leave or wait', link: '/posts/essay/Leave_or_wait.md'},
+          				{ text: '杂篇六则', link: '/posts/essay/miscellaneous_notes.md'},
+          				{ text: '军训随笔', link: '/posts/essay/militaryTraining.md' }
+        			]
+      			},
+      	// ========== 短篇小说 ==========
+      			{
+        			text: '短篇小说',
+        			collapsed: false,    
+        			items: [
+          				{ text: '余响', link: '/posts/novel/echo.md' },
+		  				{ text: '三勺石绿，半勺藤黄', link: '/posts/novel/三勺石绿，半勺藤黄.md'},
+		  				{ text: '衔香记：一瓣栀子渡余生', link: '/posts/novel/衔香记：一瓣栀子渡余生.md' },
+          				{ text: '未命名故事', link: '/posts/novel/story.md' }
+        			]
+      			}]
       
+		},
+		
+	search: {
+		provider: 'local',
+      	options: {
+        	locales: {
+          		zh: {
+            		translations: {
+              			button: {
+                			buttonText: '搜索文档',
+                			buttonAriaLabel: '搜索文档'
+              			},
+              			modal: {
+                			noResultsText: '无法找到相关结果',
+                			resetButtonTitle: '清除查询条件',
+                			footer: {
+                  				selectText: '选择',
+                  				navigateText: '切换'
+                			}
+              			}
+            		}
+				}
+			}
+		}
 	}
   }
 }))
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,9 @@
+---
+prev: false
+next: false
+lastUpdated: false
+---
+
 # 江畔晚吟时的博客  
 
 <div class="hero-section">  
@@ -19,12 +25,12 @@
   <div class="card">  
     <h3>三勺石绿，半勺藤黄</h3>  
     <p>江南梅雨时节，拓印匠人阿竹在挚友阿舟因流言与生计离去后，于孤独坚守中，通过为乡邻拓印承载记忆的老物件，最终领悟手艺的真谛并非对抗时代，而是守护人心深处无法被机器复制的温度与记忆。</p>  
-    <a href="/posts/novel/三勺石绿，半勺藤黄.md" class="card-link">阅读全文 →</a>  
+    <a href="/posts/novel/三勺石绿，半勺藤黄" class="card-link">阅读全文</a>  
   </div>  
   <!-- 文章 2 -->  
   <div class="card">  
     <h3>基于 C51 单片机的俄罗斯方块</h3>  
     <p>矩阵键盘 + 状态机设计全流程</p>  
-    <a href="/posts/project/C51俄罗斯方块" class="card-link">阅读全文 →</a>  
+    <a href="/posts/project/C51俄罗斯方块" class="card-link">阅读全文</a>  
   </div>  
 </div>  
